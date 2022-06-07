07b9a3d7a5ddf83a2ecab3831cc6a0d0d1d899e5
mariohood <manhattan199@hotmail.com>
2022-06-07 16:58:49 -0300

    implementado circuit breaker

--- src/main/java/edu/ifrs/Fault.java
+++ src/main/java/edu/ifrs/Fault.java
@@ -10,6 +10,7 @@ import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.faulttolerance.Asynchronous;
 import org.eclipse.microprofile.faulttolerance.Bulkhead;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.faulttolerance.Timeout;
@@ -19,6 +20,7 @@ import org.jboss.resteasy.specimpl.ResponseBuilderImpl;
 
 @Path("/fault")
 public class Fault {
+    int count = 1;
     @GET
     @Path("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
@@ -51,5 +53,27 @@ public class Fault {
     @Bulkhead(2)
     public String bulkhead(@PathParam("name") String name) {
             return name;
+    }
+
+    @GET
+    @Path("/cb/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Retry(maxRetries = 3, delay = 1000)
+    @CircuitBreaker(requestVolumeThreshold = 4, failureRatio = 0.75, delay = 5000)
+    public String cb(@PathParam("name") String name) {
+       
+        String message = "";
+        if(count % 2 == 0) {
+            message = "ok PAR";
+        } else {
+            message = "ok nao par";
+            ResponseBuilderImpl builder = new ResponseBuilderImpl();
+            builder.status(Response.Status.INTERNAL_SERVER_ERROR);
+            builder.entity("The requested was an error");
+            Response response = builder.build();
+            throw new WebApplicationException(response);            
+        }
+        count++;
+        return message;
     }
 }
